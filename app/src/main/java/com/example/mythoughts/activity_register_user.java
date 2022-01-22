21e28faad18adc894a11b2156f3dcc461fa8b459
mabbas <mabbasctn112@gmail.com>
2022-01-22 14:58:51 +0500

    register user activity inputs fields are set now

--- app/src/main/java/com/example/mythoughts/activity_register_user.java
+++ app/src/main/java/com/example/mythoughts/activity_register_user.java
@@ -2,10 +2,19 @@ package com.example.mythoughts;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class activity_register_user extends AppCompatActivity {
-    private FirebaseAuth mAuth;
+//import com.google.firebase.auth.FirebaseAuth;
+
+public class activity_register_user extends AppCompatActivity implements View.OnClickListener {
+    //private FirebaseAuth mAuth;
     private EditText edittext_name,edittext_email, edittext_password;
     private TextView banner;
     private Button register_user;
