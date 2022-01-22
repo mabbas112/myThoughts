e3b52da48c35e73859e846f9be16e0d4acb05b44
mabbas <mabbasctn112@gmail.com>
2022-01-22 13:59:11 +0500

    register user activity added

---
+++ app/src/main/java/com/example/mythoughts/activity_register_user.java
@@ -0,0 +1,14 @@
+package com.example.mythoughts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_register_user extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_user);
+    }
+}
