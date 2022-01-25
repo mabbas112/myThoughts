3b510d70cfb9c07b081b0f8fce5ff4483451d646
mabbas <mabbasctn112@gmail.com>
2022-01-25 19:39:23 +0500

    adding dependencies, retrieve data from firebase, add home activity, recycler view implementation.

--- app/src/main/java/com/example/mythoughts/activity_home.java
+++ app/src/main/java/com/example/mythoughts/activity_home.java
@@ -2,22 +2,69 @@ package com.example.mythoughts;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.example.mythoughts.Adapter.PostAdapter;
+import com.example.mythoughts.Model.PostModel;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class activity_home extends AppCompatActivity {
 
     FirebaseAuth auth;
+    RecyclerView recyclerView;
+    PostAdapter postAdapter;
+    List<PostModel> postModelList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         auth=FirebaseAuth.getInstance();
+        recyclerView=findViewById(R.id.recyclerView);
+
+        LinearLayoutManager layoutManager=new LinearLayoutManager(this);
+        layoutManager.setStackFromEnd(true);
+        layoutManager.setReverseLayout(true);
+        recyclerView.setLayoutManager(layoutManager);
+
+         postModelList = new ArrayList<>();
+
+         loadPosts();
+    }
+
+    private void loadPosts() {
+        DatabaseReference ref= FirebaseDatabase.getInstance().getReference("Posts");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                postModelList.clear();
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    PostModel postModel=ds.getValue(PostModel.class);
+                    postModelList.add(postModel);
+                    postAdapter=new PostAdapter(activity_home.this,postModelList);
+                    recyclerView.setAdapter(postAdapter);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(activity_home.this,""+ error,Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
     @Override
